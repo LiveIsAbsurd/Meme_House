2961a4987d1b959152fddfd8708b7c1764a46de8
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-07-14 14:36:16 +0300

    https

--- src/js/getUsers.js
+++ src/js/getUsers.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('.header-container').querySelector('h1');
 const chatMembersCount = document.createElement('p');
 
-fetch('http://77.246.96.226:3001/sendUsersCount')
+fetch('https://77.246.96.226:3001/sendUsersCount')
     .then(request => request.json())
     .then(data => {
         chatMembersCount.textContent = `Нас уже ${data.result}, стань ${data.result + 1}!`;
