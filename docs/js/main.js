23476759724318c59237783d8ec30672889f0baa
LiveIsAbsurd <130514590+LiveIsAbsurd@users.noreply.github.com>
2023-07-14 20:47:11 +0300

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -64,7 +64,7 @@ fetch('https://v2009105.hosted-by-vdsina.ru:3001/sendAdminList').then(request =>
 
 const header = document.querySelector('.header-container').querySelector('h1');
 const chatMembersCount = document.createElement('p');
-fetch('https://v2009105.hosted-by-vdsina.ru1:3001/sendUsersCount').then(request => request.json()).then(data => {
+fetch('https://v2009105.hosted-by-vdsina.ru:3001/sendUsersCount').then(request => request.json()).then(data => {
   chatMembersCount.textContent = `Нас уже ${data.result}, стань ${data.result + 1}!`;
   chatMembersCount.style = 'margin-top: 5px';
   header.appendChild(chatMembersCount);
