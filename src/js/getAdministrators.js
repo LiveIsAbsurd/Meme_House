88e5591a0b96162385195fbe87b4d7a7e1e1baf2
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-07-20 21:14:07 +0300

    формат

--- src/js/getAdministrators.js
+++ src/js/getAdministrators.js
@@ -7,105 +7,105 @@ const adminDescription = popup.querySelector(".popup__admin-description");
 const popupClose = popup.querySelector(".popup__close-button");
 
 popupClose.addEventListener("click", () => {
-    popup.classList.add("popup--hidden");
-    popupAdminPhoto.classList.remove("admin-list__vip-user");
-    popupAdminPhoto.classList.remove('admin-list__slaziet');
+  popup.classList.add("popup--hidden");
+  popupAdminPhoto.classList.remove("admin-list__vip-user");
+  popupAdminPhoto.classList.remove("admin-list__slaziet");
 });
 
 fetch("https://v2009105.hosted-by-vdsina.ru:3001/sendAdminList")
-    .then((request) => request.json())
-    .then((admins) => {
-        admins.result.forEach((element) => {
-            fetch(
-                `https://v2009105.hosted-by-vdsina.ru:3001/sendAdminInfo/${element.user.id}`
-            )
-                .then((response) => response.json())
-                .then((data) => {
-                    const photoInfo = data.result.photos[0][0];
+  .then((request) => request.json())
+  .then((admins) => {
+    admins.result.forEach((element) => {
+      fetch(
+        `https://v2009105.hosted-by-vdsina.ru:3001/sendAdminInfo/${element.user.id}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const photoInfo = data.result.photos[0][0];
 
-                    fetch(
-                        `https://v2009105.hosted-by-vdsina.ru:3001/sendAdminPhotoInfo/${photoInfo.file_id}`
-                    )
-                        .then((response) => response.blob())
-                        .then((img) => {
-                            const imgUrl = URL.createObjectURL(img);
+          fetch(
+            `https://v2009105.hosted-by-vdsina.ru:3001/sendAdminPhotoInfo/${photoInfo.file_id}`
+          )
+            .then((response) => response.blob())
+            .then((img) => {
+              const imgUrl = URL.createObjectURL(img);
 
-                            const admin = document.createElement("li");
-                            admin.classList.add("admin-list__element");
+              const admin = document.createElement("li");
+              admin.classList.add("admin-list__element");
 
-                            const adminPhoto = document.createElement("img");
-                            adminPhoto.classList.add("admin-list__photo");
-                            adminPhoto.src = imgUrl;
+              const adminPhoto = document.createElement("img");
+              adminPhoto.classList.add("admin-list__photo");
+              adminPhoto.src = imgUrl;
 
-                            if (element.user.id == '261749882') {
-                                adminPhoto.classList.add('admin-list__vip-user');
-                            }
+              if (element.user.id == "261749882") {
+                adminPhoto.classList.add("admin-list__vip-user");
+              }
 
-                            if (element.user.id == "300711096") {
-                                adminPhoto.classList.add('admin-list__slaziet');
-                            }
+              if (element.user.id == "300711096") {
+                adminPhoto.classList.add("admin-list__slaziet");
+              }
 
-                            admin.appendChild(adminPhoto);
+              admin.appendChild(adminPhoto);
 
-                            const adminName = document.createElement("p");
-                            adminName.classList.add("admin-list__name");
-                            if (element.user.username == "meme_house_bot") {
-                                adminName.textContent = "Meme House";
-                            } else {
-                                adminName.textContent = element.user.first_name;
-                            }
+              const adminName = document.createElement("p");
+              adminName.classList.add("admin-list__name");
+              if (element.user.username == "meme_house_bot") {
+                adminName.textContent = "Meme House";
+              } else {
+                adminName.textContent = element.user.first_name;
+              }
 
-                            const adminCastomName = document.createElement("p");
-                            adminCastomName.classList.add("admin-list__admin-castom-name");
-                            adminCastomName.textContent = element.custom_title;
+              const adminCastomName = document.createElement("p");
+              adminCastomName.classList.add("admin-list__admin-castom-name");
+              adminCastomName.textContent = element.custom_title;
 
-                            admin.appendChild(adminName);
-                            admin.appendChild(adminCastomName);
+              admin.appendChild(adminName);
+              admin.appendChild(adminCastomName);
 
-                            adminList.appendChild(admin);
+              adminList.appendChild(admin);
 
-                            admin.addEventListener("click", () => {
-                                setAdminPopup(
-                                    imgUrl,
-                                    element.user.first_name,
-                                    element.custom_title,
-                                    element.user.id,
-                                    element.user.username
-                                );
-                            });
-                        });
-                });
+              admin.addEventListener("click", () => {
+                setAdminPopup(
+                  imgUrl,
+                  element.user.first_name,
+                  element.custom_title,
+                  element.user.id,
+                  element.user.username
+                );
+              });
+            });
         });
     });
+  });
 
 function setAdminPopup(img, name, castomName, userId, userName) {
-    let windowHeight = window.innerHeight;
-    let popupHeight = popup.offsetHeight;
-    let scrollY = window.scrollY;
+  let windowHeight = window.innerHeight;
+  let popupHeight = popup.offsetHeight;
+  let scrollY = window.scrollY;
 
-    let topOffset = (windowHeight - popupHeight) / 2 + scrollY;
-    popup.style.top = topOffset + "px";
+  let topOffset = (windowHeight - popupHeight) / 2 + scrollY;
+  popup.style.top = topOffset + "px";
 
-    popupAdminPhoto.src = img;
-    popupAdminName.textContent = name;
-    popupAdminName.href = `https://${userName}.t.me`;
-    popupAdminCastomName.textContent = castomName;
+  popupAdminPhoto.src = img;
+  popupAdminName.textContent = name;
+  popupAdminName.href = `https://${userName}.t.me`;
+  popupAdminCastomName.textContent = castomName;
 
-    if (userId == '261749882') {
-        popupAdminPhoto.classList.add('admin-list__vip-user');
-    }
+  if (userId == "261749882") {
+    popupAdminPhoto.classList.add("admin-list__vip-user");
+  }
 
-    if (userId == "300711096") {
-        popupAdminPhoto.classList.add('admin-list__slaziet');
-    }
+  if (userId == "300711096") {
+    popupAdminPhoto.classList.add("admin-list__slaziet");
+  }
 
-    fetch(
-        `https://v2009105.hosted-by-vdsina.ru:3001/sendAdminDescription/${userId}`
-    )
-        .then((response) => response.json())
-        .then((description) => {
-            adminDescription.textContent = description;
-            popup.classList.remove("popup--hidden");
-        })
-        .catch((err) => console.log(err));
+  fetch(
+    `https://v2009105.hosted-by-vdsina.ru:3001/sendAdminDescription/${userId}`
+  )
+    .then((response) => response.json())
+    .then((description) => {
+      adminDescription.textContent = description;
+      popup.classList.remove("popup--hidden");
+    })
+    .catch((err) => console.log(err));
 }
--- src/js/getUsers.js
+++ src/js/getUsers.js
@@ -4,8 +4,9 @@ const chatMembersCount = document.createElement("p");
 fetch("https://v2009105.hosted-by-vdsina.ru:3001/sendUsersCount")
   .then((request) => request.json())
   .then((data) => {
-    chatMembersCount.textContent = `Нас уже ${data.result}, стань ${data.result + 1
-      }!`;
+    chatMembersCount.textContent = `Нас уже ${data.result}, стань ${
+      data.result + 1
+    }!`;
     chatMembersCount.style = "margin-top: 5px";
     header.appendChild(chatMembersCount);
   });
